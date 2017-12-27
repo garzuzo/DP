6f92d0271f4e54ca0b20f8b3dc3379020634990e
JohnatanG <JohnatanG@DESKTOP-3HPM1MF>
2017-12-26 23:07:57 -0500

    just another class...

---
+++ TODO/src/aDProblem.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class aDProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner lector=new Scanner(System.in);
+	}
+
+}
